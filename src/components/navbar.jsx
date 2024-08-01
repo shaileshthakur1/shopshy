61caa03c55b8f8fc93af331c29048fb3b49b6894
Shailesh Thakur <thakurshaileshkumar507@gmail.com>
2024-08-01 15:32:10 +0530

    added hamburger icon for mobile responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "@phosphor-icons/react";
 import "./navbar.css";
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navbar">
-      <div className="links">
-        <Link to="/"> Shop </Link>
-        <Link to="/contact"> Contact </Link>
-        <Link to="/cart">
+      <div className="logo">SHOPSHY</div>
+      <div className="hamburger" onClick={toggleMenu}>
+        &#9776; {/* Hamburger icon */}
+      </div>
+      <div className={`links ${menuOpen ? 'active' : ''}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}> Shop </Link>
+        <Link to="/contact" onClick={() => setMenuOpen(false)}> Contact </Link>
+        <Link to="/cart" onClick={() => setMenuOpen(false)}>
           <ShoppingCart size={32} />
         </Link>
       </div>
